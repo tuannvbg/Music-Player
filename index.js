c41c5e4802b7f518e3ee1e65ec4e7fb67ca7ac83
Alex Satchell <alex@alexs-mbp.home>
2019-12-06 17:31:56 -0500

    changes

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const previous = document.getElementById('previous');
 const play = document.querySelector('.play')
 const pause = document.querySelector('.pause');
 const next = document.getElementById('next');
+const like = document.querySelector('.like');
+const unlike = document.querySelector('.unlike');
 
 previous.addEventListener('click', function() {
-    console.log('previous button clicked');
+    console.log('previous');
 });
 
 next.addEventListener('click', function() {
-    console.log('next button clicked');
+    console.log('next');
 });
 
 play.addEventListener('click', function() {
@@ -19,6 +21,14 @@ pause.addEventListener('click', function() {
         pauseTrack();
 })
 
+like.addEventListener('click', function() {
+    likeTrack();
+});
+
+unlike.addEventListener('click', function() {
+    unlikeTrack();
+});
+
 const playTrack = () => {
     play.classList.toggle('active');
     pause.classList.toggle('active');
@@ -28,7 +38,7 @@ const playTrack = () => {
 const pauseTrack = () => {
     pause.classList.toggle('active');
     play.classList.toggle('active');
-    console.log('track paused   ');
+    console.log('track paused');
 }
 
 const nextTrack = () => {
@@ -39,3 +49,14 @@ const previousTrack = () => {
 
 }
 
+const likeTrack = () => {
+    like.classList.toggle('active');
+    unlike.classList.toggle('active');
+    console.log('song liked');
+}
+
+const unlikeTrack = () => {
+    unlike.classList.toggle('active');
+    like.classList.toggle('active');
+    console.log('song unliked');
+}
